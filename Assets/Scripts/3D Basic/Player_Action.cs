704c553260f8e9a3076ceea154279e845767b64b
bin0343-admin <bin0343@naver.com>
2025-07-22 15:30:25 +0900

    블렌드트리 추가(걷기, 뛰기), 서브스테이트머신(idle 랜덤재생)

--- Assets/Scripts/3D Basic/Player_Action.cs
+++ Assets/Scripts/3D Basic/Player_Action.cs
@@ -7,6 +7,8 @@ public class Player_Action : MonoBehaviour
     [SerializeField]
     private GameObject Player;
     private Animator Animator;
+    private float IdleTimer = 0f;
+    private float IdleDelay = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,7 @@ public class Player_Action : MonoBehaviour
     {
         Shield();
         Attack();
+        Idle();
     }
     
     void Attack()
@@ -46,6 +49,25 @@ public class Player_Action : MonoBehaviour
         else
         {
             Animator.SetBool("IsShield", false);
+        }
+    }
+
+    void Idle()
+    {
+        if (Animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle")) // Idle_SubSM에 태그 "Idle"을 붙여두자
+        {
+            IdleTimer += Time.deltaTime;
+            if (IdleTimer >= IdleDelay)
+            {
+                int rand = Random.Range(1, 4);
+                Animator.SetInteger("RandomIdleIndex", rand);
+                IdleTimer = 0;
+            }
+        }
+        else
+        {
+            IdleTimer = 0;
+            Animator.SetInteger("RandomIdleIndex", 0);
         }
     }
 }
--- Assets/Scripts/3D Basic/Player_Move.cs
+++ Assets/Scripts/3D Basic/Player_Move.cs
@@ -11,7 +11,9 @@ public class Player_Move : MonoBehaviour
     //private float CameraPitch = 0f;
 
     [SerializeField]
-    private float CharacterSpeed = 5.0f; // 캐릭터 속도
+    private float CharacterSpeed = 2.0f; // 캐릭터 속도
+    [SerializeField]
+    private float CharacterRunSpeed = 9.0f; // 달리기 속도
     [SerializeField]
     private Transform CharacterBody; // 메인 캐릭터
     [SerializeField]
@@ -42,7 +44,44 @@ public class Player_Move : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         bool IsMoving = moveInput.magnitude != 0;
+        bool IsRunning = Input.GetKey(KeyCode.LeftAlt) && IsMoving;
+        Animator.SetBool("IsRunning", IsRunning);
         Animator.SetBool("IsMoving", IsMoving);
+
+        float currentSpeed = IsRunning ? CharacterRunSpeed : CharacterSpeed;
+
+        float forwardWeight = 1.0f;
+        float backwardWeight = 0.6f;
+        float sideWeight = 0.8f;
+
+        float directionSpeedWeight = 1.0f;
+
+        if (Mathf.Abs(moveInput.x) > 0 && Mathf.Abs(moveInput.y) == 0)
+        {
+            // 좌우 이동
+            directionSpeedWeight = sideWeight;
+        }
+        else if (moveInput.y < 0)
+        {
+            // 뒤로 이동
+            directionSpeedWeight = backwardWeight;
+        }
+        else if (moveInput.y > 0 && Mathf.Abs(moveInput.x) == 0)
+        {
+            // 정면 이동
+            directionSpeedWeight = forwardWeight;
+        }
+        else if (moveInput.x != 0 && moveInput.y != 0)
+        {
+            // 대각선이면 평균값 또는 보수적 가중치 사용
+            float vertical = moveInput.y > 0 ? forwardWeight : backwardWeight;
+            directionSpeedWeight = (sideWeight + vertical) / 2f;
+        }
+
+        float adjustedSpeed = currentSpeed * directionSpeedWeight;
+
+        Animator.SetFloat("Horizontal", moveInput.x);
+        Animator.SetFloat("Vertical", moveInput.y);
 
         if (IsMoving)
         {
@@ -51,7 +90,7 @@ public class Player_Move : MonoBehaviour
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
 
             CharacterBody.forward = lookForward; // 이동할 때 이동방향 바라보게 세팅
-            transform.position += moveDir * Time.deltaTime * CharacterSpeed; // 이동
+            transform.position += moveDir * Time.deltaTime * adjustedSpeed; // 이동
         }
     }
 
