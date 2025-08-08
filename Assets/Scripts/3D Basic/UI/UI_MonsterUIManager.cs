e6427a673702d3f8d390520f9647ab0ccc933dbd
bin0343-admin <bin0343@naver.com>
2025-08-08 14:54:29 +0900

    몬스터에게 접근시 UI생성(슬라이더 대신 이미지로 바꿀것)

--- Assets/Scripts/3D Basic/UI/UI_MonsterUIManager.cs
+++ Assets/Scripts/3D Basic/UI/UI_MonsterUIManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class UI_MonsterUIManager : MonoBehaviour
 {
     //public GameObject monsterHPPrefab; // UI 프리팹
+    private Transform player;
     public Transform monsterHPPanel;   // Canvas 내 패널 참조
     public float displayDistance = 10f;
 
@@ -15,6 +16,12 @@ public class UI_MonsterUIManager : MonoBehaviour
 
     private void Start()
     {
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            player = playerObj.transform;
+        }
+
         monsterHPPanel.gameObject.SetActive(false); 
         hpSlider = monsterHPPanel.GetComponentInChildren<Slider>();
         nameText = monsterHPPanel.GetComponentInChildren<Text>();
@@ -26,14 +33,14 @@ public class UI_MonsterUIManager : MonoBehaviour
 
         if (nearest != null)
         {
-            float distance = Vector3.Distance(transform.position, nearest.transform.position);
+            float distance = Vector3.Distance(player.position, nearest.transform.position);
             if (distance <= displayDistance)
             {
                 if (!monsterHPPanel.gameObject.activeSelf)
                     monsterHPPanel.gameObject.SetActive(true);
 
                 Enemy_Stat stat = nearest.GetComponent<Enemy_Stat>();
-                hpSlider.value = stat.CurrentHP / stat.MaxHP;
+                hpSlider.value = (float)stat.CurrentHP / stat.MaxHP;
                 nameText.text = stat.EnemyName;
 
                 currentTarget = nearest;
